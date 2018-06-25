16c353c340a479a5f9c8a78d67702907a5f59ed4
Andrew Boyd <ab@ajwebsolutions.com>
2018-06-26 04:37:59 +1000

    implemented testStarted and functionality to manage testWords list

--- look-cover-check/src/store/WordState.ts
+++ look-cover-check/src/store/WordState.ts
@@ -1,8 +1,15 @@
+export interface IWordTested {
+    word: string;
+    answer: string;
+}
+
 export default class WordState {
     public words: string[];
     public testStarted: boolean;
+    public testWords: IWordTested[];
 
     constructor() {
         this.words = [];
+        this.testWords = [];
     }
 }
--- look-cover-check/src/store/reducers.ts
+++ look-cover-check/src/store/reducers.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { WordAction } from './actions';
 import { TypeKeys } from './constants';
-import WordState from './WordState';
+import { IWordTested } from './WordState';
 
 export const words = (state: string[] = [], action: WordAction) => {
     switch (action.type) {
@@ -19,17 +19,51 @@ export const words = (state: string[] = [], action: WordAction) => {
     return state;
 }
 
-export const test = (state: WordState = new WordState(), action: WordAction) => {
+export const testStarted = (state: boolean = false, action: WordAction) => {
     switch (action.type) {
         case TypeKeys.START_TEST:
-            state.testStarted = true;             
-            break;        
+            return true;             
+    }
+    return state;
+}
+
+export const testTimer = (state: number = 3, action: WordAction) => {
+    switch (action.type) {
+        case TypeKeys.START_TEST:
+            return 3;
+    }
+    return state;
+}
+
+export const testWords = (state: IWordTested[] = [], action: WordAction): IWordTested[] => {
+    switch (action.type) {
+        case TypeKeys.ADD_WORD:
+            let hasWord = false;
+            state.forEach(word => {
+                for (const key in word)
+                {
+                    if (key.toLocaleLowerCase() === action.word.toLocaleLowerCase())
+                    {
+                        hasWord = true;
+                    }
+                }
+            });
+
+            if (!hasWord) {
+                return [...state,
+                    { word: action.word, answer: ''}];
+            }
+            break;
+        case TypeKeys.REMOVE_WORD:
+            return state.filter(w => w.word.toLocaleLowerCase() !== action.word.toLocaleLowerCase());;
     }
     return state;
 }
 
 const LCCApp = combineReducers({
-    test,
+    testStarted,
+    testTimer,
+    testWords,
     words
 });
 
